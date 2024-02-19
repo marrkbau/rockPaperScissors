92cb543408168a9fa69c0a2e65ed29fb115cd3b6
B-Mar004 <bmarquez@frba.utn.edu.ar>
2024-02-19 06:12:12 -0300

    Estilado casi acabado y contador añadido

--- main.js
+++ main.js
@@ -1,50 +1,72 @@
-let elements = document.querySelectorAll(".btn");
+const buttons = document.querySelectorAll(".btn");
+const wins = document.querySelector(".wins");
+const loses = document.querySelector(".loses");
+const ties = document.querySelector(".ties");
 
-
-
-elements.forEach( (btn) => {
+buttons.forEach( (btn) => {
     btn.addEventListener('click', (event) => {
 
         
+        let txtWinsAct = wins.textContent;
+        let txtLosesAct = loses.textContent;
+        let txtTiesAct = ties.textContent;
+        let numWinsAct = parseInt(txtWinsAct);
+        let numLosesAct = parseInt(txtLosesAct);
+        let numTiesAct = parseInt(txtTiesAct);
 
         let computerChoice = decideFate();
 
         if(event.target.id === 'scissors'){
             if(computerChoice === 'scissors'){
                 alert(`You chose ${event.target.id}, the computer chose ${computerChoice} it's a tie.`)
+                numTiesAct++;
             }
             if(computerChoice === 'paper'){
                 alert(`You chose ${event.target.id}, the computer chose ${computerChoice} you win!`)
-            }
+                numWinsAct++;
+            }   
             if(computerChoice === 'rock'){
                 alert(`You chose ${event.target.id}, the computer chose ${computerChoice} you lose.`)
+                numLosesAct++;
             }
         }
         if(event.target.id === 'rock'){
             if(computerChoice === 'rock'){
+                numTiesAct++;
                 alert(`You chose ${event.target.id}, the computer chose ${computerChoice} it's a tie.`)
             }
             if(computerChoice === 'scissors'){
                 alert(`You chose ${event.target.id}, the computer chose ${computerChoice} you win!`)
+                numWinsAct++;
             }
             if(computerChoice === 'paper'){
                 alert(`You chose ${event.target.id}, the computer chose ${computerChoice} you lose.`)
+                numLosesAct++;
             }
         }
         if(event.target.id === 'paper'){
             if(computerChoice === 'paper'){
+                numTiesAct++;
                 alert(`You chose ${event.target.id}, the computer chose ${computerChoice} it's a tie.`)
             }
             if(computerChoice === 'rock'){
                 alert(`You chose ${event.target.id}, the computer chose ${computerChoice} you win!`)
+                numWinsAct++;
             }
             if(computerChoice === 'scissors'){
                 alert(`You chose ${event.target.id}, the computer chose ${computerChoice} you lose.`)
+                numLosesAct++;
             }
+
+           
         }
         // const resultParagraph = document.createElement('p');
         // resultParagraph.textContent = 'Computer Choice: ' + computerChoice;
         // document.body.appendChild(resultParagraph);
+
+        wins.textContent = numWinsAct.toString();
+        loses.textContent = numLosesAct.toString();
+        ties.textContent = numTiesAct.toString();
         })
 })
 
